4e776f549986f72fe0e46abb873de838edcbc79c
Arthur Oliveira <103907096+thisarthur@users.noreply.github.com>
2022-06-13 18:28:44 -0300

    Add files via upload
    Código otimizado para scrapping

---
+++ botConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    olxAnnouncementSelector: {
+
+    productName : '#content .h3us20-0 .sc-45jt43-0',
+    value : '#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.cpscHx > div.h3us20-6.jUPCvE > div > div > div.sc-hmzhuo.dtdGqP.sc-jTzLTM.iwtnNi > div.sc-hmzhuo.sc-12l420o-0.kUWFYY.sc-jTzLTM.iwtnNi > h2.sc-ifAKCX.eQLrcK',
+    publicationData : '#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.gyKyRK > div.h3us20-6.hzUJDA > div > div > div > span.sc-1oq8jzc-0.jvuXUB.sc-ifAKCX.fizSrB',
+    announcementName : '#miniprofile > div > div > div.sc-ccLTTT.sc-igwadP.ghgDyF > div.sc-ckYZGd.jylJIb.sc-jTzLTM.iwtnNi > div > div.sc-cnTzU.iMHUBS.sc-jTzLTM.iwtnNi > span'
+}
+}   
+
+
--- index.js
+++ index.js
@@ -1,105 +1,78 @@
- 
-const axios = require ('axios');
-const cheerio = require ('cheerio');
-const fs = require ('fs');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
+const { olxAnnouncementSelector: { productName, value, publicationData, announcementName } } = require('./botConfig');
 
-//Site Pai que será extraido os dados!
-const siteAlvo = 'https://sp.olx.com.br/imoveis'
+//percorrer páginas
+async function main() {
+    let numPag = 100
+    let count = 1
 
-const dados = [];
-let totpgpai = 100
-let count = 1
+    while (count <= numPag) {
+        const url = `https://sp.olx.com.br/imoveis?o=${count}`
 
+        //extrair código fonte e links do HTML pág Alvo
+        async function getLinksOnThePage() {
+            try {
+                const res = await axios.get(url);
+                const html = await res.data;
+                const $ = await cheerio.load(html);
 
-async function main(){   //percorrer paginas
-    while (count <= totpgpai){
-        const urlfilho = `https://sp.olx.com.br/imoveis?o=${count}`
-       
-  
+                const links = []
+                $('.sc-12rk7z2-1').each(function (i, lnk) {
+                    links[i] = $(lnk).attr('href');
+                })
+                links.forEach(function (allLinks) {
+                    getPageContent(allLinks)
+                })
+                //seletor com a classe em que estao os links, each para percorrer a array, array links recebendo os links
+                //forEach percorrendo todos os links chamando a funcao getpagecontent
+                return links;
 
+            } catch (error) {
+                console.log('Erro ao gerar links!: ' + error);
+            }};
 
-//extrair código fonte do HTML pág Pai
-const dadosBrutos = async () => {
-    try {
-        
-        const res = await axios.get(urlfilho);
-        return res.data;
-        
-    } catch (error) {
-        console.log('Erro ao extrair dados brutos!: ' + error);
-    }
-};
+        //Extrair dados do anúncio
+        async function getPageContent(announcementLink) {
+            try {
+                const res = await axios.get(announcementLink); //axios pega o html dos links extraidos
+                const html = res.data;
+                const $ = await cheerio.load(html); //instancia cheerio
 
-//Extrair links da pág Pai
-const listaLinks = async () => {
-    const html = await dadosBrutos(); //recebe codigo fonte html de dadosBrutos
-    const $ = await cheerio.load(html); //instancia cheerio
-    $('.sc-12rk7z2-1').each(function(i, lnk){
-        dados [i] = $(lnk).attr('href');
-    }) 
-    //seletor com a classe em que estao os links, each para percorrer a array, array dados recebendo os links
+                //obj puxando seletores do botconfig
+                announcement = {
+                    productName: $(productName).text(),
+                    value: $(value).text(),
+                    publicationData: $(publicationData).text(),
+                    announcementName: $(announcementName).text()
+                }
 
-       return dados;
-}
+                //const aninhando dados para serem impressos.
+                const printAnnouncement = `
+                    Produto:${announcement.productName}
+                    Valor:${announcement.value}
+                    Anunciante:${announcement.announcementName}
+                    ${announcement.publicationData}
+                    Link: ${announcementLink}
+                    `
+                WriteToFile(printAnnouncement);
 
-//Extrair dados do anúncio
-const coletaDados = async(pg) =>{
-  
-    try {
-        const res = await axios.get(pg); //axios pega o html da pág filho
-        const html = res.data;              
-        const $ = await cheerio.load(html); //instancia cheerio
-        let nomeProduto = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.gyKyRK > div.h3us20-6.gFNxVM > div > div > h1').text()
-        let valor = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.cpscHx > div.h3us20-6.jUPCvE > div > div > div.sc-hmzhuo.dtdGqP.sc-jTzLTM.iwtnNi > div.sc-hmzhuo.sc-12l420o-0.kUWFYY.sc-jTzLTM.iwtnNi > h2.sc-ifAKCX.eQLrcK').text()
-        let publi = $('#content > div.sc-18p038x-3.dSrKbb > div > div.sc-bwzfXH.h3us20-0.cBfPri > div.duvuxf-0.h3us20-0.gyKyRK > div.h3us20-6.hzUJDA > div > div > div > span.sc-1oq8jzc-0.jvuXUB.sc-ifAKCX.fizSrB').text()
-        let anunciante = $('#miniprofile > div > div > div > div.sc-cbMPqi.eTGDLQ.sc-jTzLTM.iwtnNi > div > span').text()
-            //var com os querry retornando as informações pedidas
+            } catch (error) {
+                console.log('Problema na extração de dados: ' + error);
+            }};
 
-        const resultado = `
-        Produto:${nomeProduto}
-        Valor:${valor}
-        Anunciante:${anunciante}
-        ${publi}
-        Link: ${pg}
-        `
-        
-        gravaTXT(resultado);
-           
-          
-    } catch (error) {
-        console.log('Problema na extração de dados: ' + error);
-    }
-};
+        //Cria um txt e coloca os valores extraídos
+        async function WriteToFile(content) {
 
-//Cria um txt e coloca os valores extraídos
-const gravaTXT = async (result) => {
-    fs.appendFile('fisgar.txt', result, {flag: 'a+'}, function(err){
-        if(err)
-        console.log('Erro na geração TXT: ' +err)
-    })
-}
-const apresentaDados = async () =>{
-    const todosLnks = await listaLinks(); //var recebendo todos os links gerados
-    todosLnks.map(function(linksFilhos){ 
-        coletaDados(linksFilhos);
-    }) //percorrendo todos os links com a funcao coletaDados
-};
+            fs.appendFile('fisgar.txt', content, { flag: 'a+' }, function (err) {
+                if (err)
+                    console.log('Erro na geração TXT: ' + err)
+            })}
+        await getLinksOnThePage()
+        await getPageContent(url)
 
-
-//const principal chamando a ultima funcao com resultado
-const pgn = async () =>{
-    await apresentaDados()
-        
-   }
-
-   pgn();
-
-   await coletaDados(urlfilho)
-   count ++
-
-}
-}
-
-//chamada de método
+        count++
+    }}
 main();
